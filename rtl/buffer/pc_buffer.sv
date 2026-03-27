0e88999c022d683742cd6d829a82093d7ba55cd4
Yoan Lagarde <yoan.lagarde@grenoble-inp.org>
2026-03-27 09:28:50 +0100

    merci roro

--- rtl/buffer/pc_buffer.sv
+++ rtl/buffer/pc_buffer.sv
@@ -1,15 +1,15 @@
-module pc_buffer_mem (
+module pc__buffer(
     input logic clk,
     input logic rst,
-    input logic [31:0] i_pcM,
-    output logic [31:0] o_pcM
+    input logic [31:0] i_pc,
+    output logic [31:0] o_pc
 );
 
 always_ff @(posedge clk or posedge rst) begin
     if (rst) begin
-      o_pcM <= 32'b0;
+      o_pc <= 32'b0;
     end else begin
-      o_pcM <= i_pcM;
+      o_pc <= i_pc;
     end
 end
 
