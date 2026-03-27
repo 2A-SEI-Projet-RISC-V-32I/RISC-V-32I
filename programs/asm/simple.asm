2a4bea7af2909d22d6edaa4be14e06cc9dd34da8
Axel TARTAVEZ <axel.tartavez@gmail.com>
2026-03-27 09:33:15 +0100

    Programme de test simple et programme.c pas encore utilisé

---
+++ programs/asm/simple.asm
@@ -0,0 +1,4 @@
+addi x5, x0, 10    # On charge la valeur 10 dans le registre x5
+addi x6, x0, 20    # On charge la valeur 20 dans le registre x6
+add  x7, x5, x6    # On additionne x5 et x6. Le registre x7 doit valoir 30
+sub  x8, x6, x5    # On soustrait (20 - 10). Le registre x8 doit valoir 10
