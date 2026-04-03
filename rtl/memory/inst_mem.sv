8590cb6d3147e7a3e79e3e67e3136fc7f8a6fa40
Axel TARTAVEZ <axel.tartavez@gmail.com>
2026-04-03 16:11:44 +0200

    Test pour 15 instructions

--- rtl/memory/inst_mem.sv
+++ rtl/memory/inst_mem.sv
@@ -23,28 +23,11 @@ module inst_mem #(
 
     // Pour le top_tb (à commenter ou non en fonction)
 
-        //$readmemh("programs/bin/simple.hex", mem);
+    $readmemh("programs/bin/inst.hex", mem);
 
     // Pour le inst_mem_tb (à commenter ou non en fonction)
 
-    mem[0] = 32'h00a00293;
-    mem[1] = 32'h01400313;
-    mem[2] = 32'h00400493;
-    mem[3] = 32'h00000013;
-    mem[4] = 32'h00000013;
-    mem[5] = 32'h00000013;
-    mem[6] = 32'h00000013;
-    mem[7] = 32'h006283b3;
-    mem[8] = 32'h00000013;
-    mem[9] = 32'h00000013;
-    mem[10] = 32'h00000013;
-    mem[11] = 32'h40530433;
-    mem[12] = 32'h00000013;
-    mem[13] = 32'h00000013;
-    mem[14] = 32'h0074a023;
-    mem[15] = 32'h00000013;
-    mem[16] = 32'h00000013;
-    mem[17] = 32'h0004a503;
+    //mem[0] = 32'h00a00293;
 
 
     end
